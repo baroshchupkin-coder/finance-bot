da39845c4cec1b34dbe4d30efef42b3dbe427de7
baroshchupkin-coder <ba.roshchupkin@gmail.com>
2026-04-23 13:37:21 +0700

    fix credentials via env

--- bot.py
+++ bot.py
@@ -11,7 +11,12 @@ TOKEN = os.getenv("BOT_TOKEN")
 scope = ["https://spreadsheets.google.com/feeds",
          "https://www.googleapis.com/auth/drive"]
 
-creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+import os
+import json
+from oauth2client.service_account import ServiceAccountCredentials
+
+creds_dict = json.loads(os.environ["GOOGLE_CREDENTIALS"])
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 client = gspread.authorize(creds)
 
 sheet = client.open("Finance bot").worksheet("requests")
